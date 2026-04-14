9c30c078636d6d02166c257c3a4de1a40b054092
Carlos Silva <123014786+CarlosSilva8@users.noreply.github.com>
2026-04-14 20:49:19 -0300

    Continuação do bloco de aula 3

--- introducao-cpp.cpp
+++ introducao-cpp.cpp
@@ -9,15 +9,23 @@ Uso o Void como forma de escrever diversos blocos de código
 e executá-los individualmenmte na função main
 Bloco 1 - Introdução ao C++               */
 void bloco1() {
-    cout << "Rodando bloco 1" << endl;
+    cout << "Rodando bloco 1:\n" << endl;
     cout << "Hello World!" << endl; // cout é a função character output que está na biblioteca iostream
     cout << "Boa tarde, " << "Carlos." << endl; // endl pula a linha
 }
 
 // Bloco 2 - Data Types, Variables and Input Output
 void bloco2() {
-    cout << "Rodando bloco 2" << endl;
-    /* Tipos de dados:
+    float x, y, z; // Variável precisa ter seu tipo declarado corretamente
+    string deus_ex_machina;
+    cout << "Rodando bloco 2:\n" << endl;
+    x = 2.1;
+    y = 0.6;
+    z = x * y; 
+    deus_ex_machina = "O que eu posso fazer?";
+    cout << deus_ex_machina<< endl << x << ", " << y << endl << z;
+    /* 
+    Tipos de dados:
     int: 1, 0, -50, 100
     float: 0.5, 3.7
     bool: true, false
@@ -25,9 +33,29 @@ void bloco2() {
     char: "a", "x", "8", ":"
 */
 }
-// Funãoo Main gerencia o inicio, execução e finalização das tarefas. Int indica que ao final da execução, um número inteiro será retornado
+/*
+Bloco 3 - User Input and Constants           */
+void bloco3(){
+   
+    cout << "Executando o Bloco 3:\n";
+     /*
+    float n;
+    const float gravidade = -9.8; // Variável constante
+    cout << "Digite um numero: " << endl;
+    cin >> n;  //character input junto com o operador de extração ">>"
+    cout << "Voce digitou: " << n << endl;
+    */
+    int n, x;
+    cout << "Digite o primeiro numero: " << endl;
+    cin >> n;
+    cout << "Digite o segundo numero: " << endl;
+    cin >> x;
+    cout << "\nResultados:\n" << n << endl << x;
+}
+
+// Função Main gerencia o inicio, execução e finalização das tarefas. Int indica que ao final da execução, um número inteiro será retornado
 int main() {
-    bloco2();
+    bloco3();
     return 0; // retorna o zero como indicativo de sucesso na execução do bloco. Process returned 0 (0x0)   execution time : 1.407 s
 
 } // Todo o bloco dentro das chaves é executado em uma run em ordem sequencial
